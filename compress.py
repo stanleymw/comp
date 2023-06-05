41718bee57758c3948febba032a1619c5143d98a
stanleymw <107821509+stanleymw@users.noreply.github.com>
2023-06-04 17:04:36 -0700

    finish compression implementation

--- compress.py
+++ compress.py
@@ -2,6 +2,7 @@
 import argparse
 import struct
 import queue
+import math
 
 parser = argparse.ArgumentParser(
                     prog='comp',
@@ -78,7 +79,10 @@ def trav(node, path):
 
 trav(root, 0)
 print("SYMBOLS:", symbols)
-
+for (i,v) in symbols.items():
+    print(f"{i}: {v:b}")
+dcode = {b:a for (a,b) in symbols.items()}
+print("DECODE:", dcode)
 # TODO: canonical coding
 # symbols = sorted(symbols, key = lambda item: len(item[1]))
 # print(symbols)
@@ -91,10 +95,53 @@ print("SYMBOLS:", symbols)
 #     symbols[i+1][1] = current_code
 # bits = "10101010"
 
+class writeable_byte:
+    def __init__(self):
+        self.value = 0
+        self.remaining_digits = 8
+
+    def append(self, bits, num_digits_of_bits):
+        # bits is a binary number
+        self.value = self.value | (bits << (self.remaining_digits - num_digits_of_bits))
+        self.remaining_digits -= num_digits_of_bits
+
+        print(f"[!] current write: {self.value:b} | bits left: {self.remaining_digits}")
+
 with open("out.txt", "ab") as binary_file:
     # Write bytes to file
     # binary_file.write(struct.pack('c', int(bits, 2)))
 
     # this is not encoded correctly: TODO: FIX
+    write_buffer = []
+    current_byte = writeable_byte()
     for byte in text:
-        binary_file.write(struct.pack('i', symbols[byte]))
+        symbol = symbols[byte]
+        if symbol == 0:
+            symbol_size = 1
+        else:
+            symbol_size = math.floor(math.log(symbol, 2)) + 1
+
+        # lets see how many bytes this will use
+        # number_of_bytes_required = (symbol_size//8) + 1
+
+        print(f"DEBUG: {symbol:b} [{symbol_size}]")
+        for bit_number in range(symbol_size, 0, -1):
+            # write each bit individually
+            this_bit = (symbol >> (bit_number-1)) % 2
+            current_byte.append(this_bit, 1)
+
+            if current_byte.remaining_digits == 0:
+                write_buffer.append(current_byte)
+                current_byte = writeable_byte()
+
+        # current_byte += (symbol_size <<
+
+        while len(write_buffer) > 0:
+            # print(f"{write_buffer.pop().value.to_bytes()}")
+            binary_file.write(write_buffer.pop().value.to_bytes())
+
+with open("out.txt", "rb") as binary_file:
+    while (by := binary_file.read(1)):
+        val = int.from_bytes(by)
+        for bit in range(8,0,-1):
+            print((val >> (bit-1)) % 2)
